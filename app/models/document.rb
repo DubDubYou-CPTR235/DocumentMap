900f3ed63644921ea043b9f0b53e9237da14de37
Luke Travis <luke.foobar@gmail.com>
2015-03-03 14:45:49 -0500

    Paperclip configured to only accept specific file formats.

--- app/models/document.rb
+++ app/models/document.rb
@@ -1,4 +1,6 @@
 class Document < ActiveRecord::Base
   belongs_to :user
   has_attached_file :upload
+  validates_attachment_presence :upload
+  validates_attachment_content_type :upload, :content_type => ['image/jpeg', 'image/png', 'image/gif', 'audio/mp4', 'audio/ogg', 'audio/flac', 'audio/mpeg', 'application/pdf', 'video/mpeg', 'video/mp4', 'video/avi', 'video/quicktime', 'video/webm', 'video/x-flv']
 end
